c2fc9019822a430d6787b79b24026757914fb456
Youssef Darahem <youssef.darahem@gmail.com>
2024-11-02 12:29:41 +0300

    added a comment to dev branch

--- Animal.java
+++ Animal.java
@@ -2,6 +2,8 @@ public class Animal {
     public String name;
     public String type;
 
+    // hello from dev
+
     public void makeSound() {
         System.out.println("Making a sound..");
     }
