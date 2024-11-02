f7c76715edec65d69a472bac9c44bee2fbc3508e
Youssef Darahem <youssef.darahem@gmail.com>
2024-11-02 12:37:10 +0300

    added a comment

--- Animal.java
+++ Animal.java
@@ -2,6 +2,7 @@ public class Animal {
     public String name;
     public String type;
 
+    // hello from main again
     public void makeSound() {
         System.out.println("Making a sound..");
     }
